a1b463ce8b858d9b7edae982643dd1aca6a8631f
Corey Mason <corey.mason98@googlemail.com>
2017-01-28 15:54:45 -0800

    Fix Air Jumping and Merged

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    [SerializeField]
+    float walkSpeed = 0.0f;
+    [SerializeField]
+    float fallSpeed = 1.0f;
+    [SerializeField]
+    float jumpPower = 1.0f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
     [System.Serializable]
     public class PlayerStats {
         public int health = 1;
@@ -11,9 +20,35 @@ public class Player : MonoBehaviour {
 
     public PlayerStats stats = new PlayerStats();
 
+    void start() {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
     void Update() {
-        if(transform.position.y <= -20) {
+        MoveHorizontal();
+        Jump();
+        if (transform.position.y <= -20) {
             DamagePlayer(1000000);
+        }
+    }
+
+    void MoveHorizontal() {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * walkSpeed, rb.velocity.y * fallSpeed) * Time.deltaTime;
+    }
+
+    void Jump() {
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            rb.AddForce(Vector2.up * jumpPower);
+        }
+    }
+
+    void OnCollisionStay(Collision coll) {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit(Collision coll) {
+        if (isGrounded) {
+            isGrounded = false;
         }
     }
 
