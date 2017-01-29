eed72bc02e911e61231cc3d5e643703e2c80bdb0
Zextillion <LookASupernova@gmail.com>
2017-01-28 19:27:01 -0800

    Added in melee attack
    Added melee attack + relevant animations

---
+++ Assets/Scripts/MeleeAttack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttack : MonoBehaviour
+{
+    [SerializeField]
+    float attackDuration = 0.5f;
+
+    private bool attacking = false;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckForAttack();
+	}
+
+    void CheckForAttack()
+    {
+        if (Input.GetButtonDown("Fire1") && Player.current.isGrounded == true)
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        if (attacking == false)
+        {
+            gameObject.transform.FindChild("MeleeHitbox").gameObject.SetActive(true);
+            Player.current.GetComponent<Animator>().SetTrigger("Attack");
+            attacking = true;
+            Invoke("DisableHitbox", attackDuration);
+        }
+    }
+
+    void DisableHitbox()
+    {
+        gameObject.transform.FindChild("MeleeHitbox").gameObject.SetActive(false);
+        attacking = false;
+    }
+}
---
+++ Assets/Scripts/PlayerDamageScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamageScript : MonoBehaviour
+{
+    [SerializeField]
+    int damage = 1;
+
+    void OnCollision2D(Collider2D hit)
+    {
+        if (hit.GetComponent<EnemyBird>() != null)
+        {
+            hit.GetComponent<EnemyBird>().DamageEnemy(damage);
+        }
+    }
+}
