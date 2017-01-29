be721e614683ccc29d85b48901efcfe868682a16
Corey Mason <corey.mason98@googlemail.com>
2017-01-28 23:12:09 -0800

    Moon

---
+++ Assets/Scripts/Moon.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moon : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z) * 1.001f;
+	}
+}
